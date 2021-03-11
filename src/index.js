bc84ccc882cdfb7190837b5a63b5a52e93d63c8d
Anna Cherkesova <cherkesova.anna@gmail.com>
2021-03-11 19:10:49 +0300

    feat: implement avg function

--- src/index.js
+++ src/index.js
@@ -1,36 +1,44 @@
 
 // Min()
 exports.min = function min (array) {
-if ( array==''|| array==null ){
-  return 0;
-}
-let min = array[0];
-let next;
-for(let i = 0; i < array.length; i++){
-	next = array[i];
-	if (min > next){
-		min = next;
-	} 
-}
-return min;
+    if ( array==''|| array==null ){
+      return 0;
+    }
+    let min = array[0];
+    let next;
+    for(let i = 0; i < array.length; i++){
+        next = array[i];
+        if (min > next){
+            min = next;
+        } 
+    }
+    return min;
 }
 
 // Max()
 exports.max = function max (array) {
-if (array.length === 0 || array === undefined){
-  return 0;
-}
-let max = array[0];
-let next;
-for(let i = 0; i < array.length; i++){
-	next = array[i];
-	if (max < next){
-		max = next;
-	} 
-}
-return max;
+    if ( array==''|| array==null ){
+        return 0;
+    }
+    let max = array[0];
+    let next;
+    for(let i = 0; i < array.length; i++){
+        next = array[i];
+        if (max < next){
+            max = next;
+        } 
+    }
+    return max;
 }
 
+// Avg()
 exports.avg = function avg (array) {
-  return 0;
+    if ( array==''|| array==null ){
+        return 0;
+    }
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+        sum = sum + array[i];
+    }
+	return sum/array.length
 }
